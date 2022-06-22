8a4efe283143187e0a54634dc7bbae978ee2eb53
ikreskota <xvborkan@gmail.com>
2022-06-22 13:25:07 +0800

    menambahkan fitur show password

--- login.js
+++ login.js
@@ -8,3 +8,28 @@ function startlog() {
         var logakses = document.getElementById('form').innerHTML = '<div class="col-lg-8 col-10 text-center"> <h2>SILAHKAN KELUAR !!!</h2> <h5>ANDA BUKAN BAGIAN DARI KRESNA DUMBO</h5> </div>';
     }
 };
+
+$('#show2').hide();
+var pwd = document.getElementById('password');
+var pwd2 = document.getElementById('password2');
+
+$('#show').on('click', function () {
+    $('#show').hide();
+    $('#show2').show();
+    if (pwd.type === "password") {
+        pwd.type = "text";
+    } else {
+        pwd.type = "password";
+    }
+});
+
+$('#show2').on('click', function () {
+    $('#show2').hide();
+    $('#show').show();
+
+    if (pwd.type === "text") {
+        pwd.type = "password";
+    } else {
+        pwd.type = "text";
+    }
+})

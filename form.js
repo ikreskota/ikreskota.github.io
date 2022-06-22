823aa56f6e023ae4b04b2da1f6d6ee7901834db6
ikreskota <xvborkan@gmail.com>
2022-06-22 11:47:12 +0800

    merubah error tidak dapat mengirim data

--- form.js
+++ form.js
@@ -1,21 +1,21 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbwvMWGjtcpzuVhB7-GxZd7B1OkY82sA3nkxM9tG2-oDMPKDS6mA8G-MeE06LKfQ56xv3g/exec';
-    const form = document.forms['form_input_lapgas'];
-    const btnKirim = document.querySelector('.btn-kirim');
-    const btnLoading = document.querySelector('.btn-loading');
-    const myAlert = document.querySelector('.my-alert');
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzenUgmXhW_8KlFzAvJHBldb-oAAlbpfZzdWTSdoCvXC9tmr0XvGC8QzVDdvm75m0BPYQ/exec';
+const form = document.forms['form_input_lapgas'];
+const btnKirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
+const myAlert = document.querySelector('.my-alert');
 
 
-    form.addEventListener('submit', e => {
-      e.preventDefault()
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  btnLoading.classList.toggle('d-none');
+  btnKirim.classList.toggle('d-none');
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then(response => {
       btnLoading.classList.toggle('d-none');
       btnKirim.classList.toggle('d-none');
-      fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-        .then(response => {
-          btnLoading.classList.toggle('d-none');
-          btnKirim.classList.toggle('d-none');
-          myAlert.classList.toggle('d-none');
-          form.reset();
-          console.log('Success!', response)
-        })
-        .catch(error => console.error('Error!', error.message));
+      myAlert.classList.toggle('d-none');
+      form.reset();
+      console.log('Success!', response)
     })
+    .catch(error => console.error('Error!', error.message));
+})

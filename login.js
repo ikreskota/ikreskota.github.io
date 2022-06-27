f075be753de76b5e13efddb95640f40b18e8d63b
ikreskota <xvborkan@gmail.com>
2022-06-27 11:30:35 +0800

    merubah icon kunci

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ function startlog() {
     var net2 = ('kresnadumbo') + ('manguni88');
     if (net1 == net2) {
         var logakses = document.getElementById('form').innerHTML =
-            '<div class="col-lg-8 col-10 text-center" data-aos="zoom-out-down" data-aos-duration="1500" data-aos-delay="500"> <a class="btn btn-primary" style:"width: 100px;" href="form.html" role="button"><i class="bi bi-unlock-fill" style="font-size: 30px;"></i></a> </div>';
+            '<div class="col-lg-8 col-10 text-center" data-aos="zoom-out-down" data-aos-duration="1500" data-aos-delay="500"> <a class="btn btn-primary" style:"width: 100px;" href="form.html" role="button"><i class="bi bi-unlock-fill" style="font-size: 30px;"></i>KLICK DISINI !</a> </div>';
     } else {
         var logakses = document.getElementById('form').innerHTML = '<div class="col-lg-8 col-10 text-center"> <h2>SILAHKAN KELUAR !!!</h2> <h5>ANDA BUKAN BAGIAN DARI KRESNA DUMBO</h5> </div>';
     }

b7a6faa8b543ba1305d6a658c15730636d27ed5d
ikreskota <xvborkan@gmail.com>
2022-07-30 21:28:17 +0800

    menambah data personil dan merubah pass

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 function startlog() {
     var net1 = document.getElementById('username').value + document.getElementById('password').value;
-    var net2 = ('kresnadumbo') + ('manguni88');
+    var net2 = ('kresnadumbo') + ('manguni');
     if (net1 == net2) {
         var logakses = document.getElementById('form').innerHTML =
             '<div class="col-lg-8 col-10 text-center" data-aos="zoom-out-down" data-aos-duration="1500" data-aos-delay="500"> <a class="btn btn-primary" style:"width: 100px;" href="form.html" role="button"><i class="bi bi-unlock-fill" style="font-size: 30px;"></i>KLICK DISINI !</a> </div>';
